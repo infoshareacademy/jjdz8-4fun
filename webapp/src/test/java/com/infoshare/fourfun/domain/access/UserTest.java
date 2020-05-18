648b92857e9118f4b3ca61d70bd269d62e1b804f
DorotaMiler <dorota.miler@outlook.com>
2020-05-19 00:44:59 +0200

    UserTest -> //TODO to check correct library implementation as @Test annotation isnot working

--- webapp/src/test/java/com/infoshare/fourfun/domain/access/UserTest.java
+++ webapp/src/test/java/com/infoshare/fourfun/domain/access/UserTest.java
@@ -3,7 +3,7 @@ package com.infoshare.fourfun.domain.access;
 import com.infoshare.fourfan.domain.access.User;
 
 public class UserTest {
-
+//TODO check Junit library correct implementation for @Test to work
     @Test
     @DisplayName("Verifies if object UserBuilder created/exists with input name an surname data provided in method's parameter")
     public void testIfUserBuilderCreatedCorrectlyForValidNameAndSurname() {

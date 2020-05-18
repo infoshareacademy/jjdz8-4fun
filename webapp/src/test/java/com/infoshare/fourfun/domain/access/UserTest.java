b3dbbff0be6ce33861740b1090fb010f93acf0f0
DorotaMiler <dorota.miler@outlook.com>
2020-05-19 00:43:44 +0200

    test creation for UserBuilder to create user basig on name & surname; //TODO to apply

---
+++ webapp/src/test/java/com/infoshare/fourfun/domain/access/UserTest.java
@@ -0,0 +1,14 @@
+package com.infoshare.fourfun.domain.access;
+
+import com.infoshare.fourfan.domain.access.User;
+
+public class UserTest {
+
+    @Test
+    @DisplayName("Verifies if object UserBuilder created/exists with input name an surname data provided in method's parameter")
+    public void testIfUserBuilderCreatedCorrectlyForValidNameAndSurname() {
+        User user = new User.UserBuilder("Piotr","Kowalski")
+                .build();
+        System.out.println(user);
+    }
+}

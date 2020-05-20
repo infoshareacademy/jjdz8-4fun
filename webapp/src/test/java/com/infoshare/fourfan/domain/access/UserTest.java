20ae4562502327abf2f220f4edbba9132aaaed08
DorotaMiler <dorota.miler@outlook.com>
2020-05-20 23:44:32 +0200

    Assertion addition to verifie if users name equals user.name - success

--- webapp/src/test/java/com/infoshare/fourfan/domain/access/UserTest.java
+++ webapp/src/test/java/com/infoshare/fourfan/domain/access/UserTest.java
@@ -1,11 +1,12 @@
 package com.infoshare.fourfan.domain.access;
 
-import com.infoshare.fourfan.domain.access.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
-//TODO check Junit library correct implementation for @Test to work
+    //TODO check Junit library correct implementation for @Test to work
     //done: typo error i package name (fourfun instead of fourfan)
     @Test
     @DisplayName("Verifies if object UserBuilder created/exists with input name an surname data provided in method's parameter")
@@ -16,8 +17,10 @@ public class UserTest {
 
         // then
 
-        User user = new User.UserBuilder("Piotr","Kowalski")
+        User user = new User.UserBuilder("Piotr", "Kowalski")
                 .build();
         System.out.println(user);
+        assertEquals("Piotr", user.getName());
+        assertEquals("Kowalski", user.getLastName());
     }
 }

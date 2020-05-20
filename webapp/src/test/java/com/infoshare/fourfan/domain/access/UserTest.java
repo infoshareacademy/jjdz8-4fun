57c41a9a510a4b3471552bfe2d8695cc6ccd9775
DorotaMiler <dorota.miler@outlook.com>
2020-05-20 22:57:21 +0200

    fund cause of error - typo error in package name (fourfun instead of fourfan-> now first Test run correctly - all passed for testIfUserBuilderCreatedCorrectlyForValidNameAndSurname())

--- webapp/src/test/java/com/infoshare/fourfan/domain/access/UserTest.java
+++ webapp/src/test/java/com/infoshare/fourfan/domain/access/UserTest.java
@@ -1,10 +1,12 @@
-package com.infoshare.fourfun.domain.access;
+package com.infoshare.fourfan.domain.access;
 
 import com.infoshare.fourfan.domain.access.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
 //TODO check Junit library correct implementation for @Test to work
+    //done: typo error i package name (fourfun instead of fourfan)
     @Test
     @DisplayName("Verifies if object UserBuilder created/exists with input name an surname data provided in method's parameter")
     public void testIfUserBuilderCreatedCorrectlyForValidNameAndSurname() {

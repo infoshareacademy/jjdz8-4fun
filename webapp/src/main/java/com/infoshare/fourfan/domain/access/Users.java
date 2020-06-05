83d8da2f3377d559e04f2b1ac298b7e9f7222876
DorotaMiler <dorota.miler@outlook.com>
2020-06-05 21:52:11 +0200

    - WelcomeServletCorrection - responce utilizing user in basic html - slight changes in Users: * acknowledgment of no users registered yet to avoid exception *containsUser() checking if user on the list * printAllUsers()

--- webapp/src/main/java/com/infoshare/fourfan/domain/access/Users.java
+++ webapp/src/main/java/com/infoshare/fourfan/domain/access/Users.java
@@ -5,16 +5,33 @@ import java.util.List;
 
 public class Users {
 
-    private List<User> users = new ArrayList<>();
+    private static List<User> users = new ArrayList<>();
 
-    public Users() {
-    }
-
-    public List<User> getUsers() {
+    public static List<User> getUsers() {
+        if (users.size() == 0) {
+            System.out.println("No users registered yet.");
+        }
         return users;
     }
 
     public void add(User user) {
         users.add(user);
     }
+
+    public void printAllUsers() {
+        for (User user : this.users) {
+            System.out.println(user);
+        }
+    }
+
+    public static boolean containsUser(User user) {
+        List<User> users = getUsers();
+        for (User userFromUsers : users) {
+            if (userFromUsers.getEmail() == user.getEmail()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
 }

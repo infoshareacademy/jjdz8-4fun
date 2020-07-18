a977a6a2c7a9097e612de12f71178af226310d37
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-07-19 00:30:09 +0200

    ProductListServlet update, add glassfish dependencies to .pom file

--- webapp/src/main/java/com/infoshare/fourfan/servlet/ProductListServlet.java
+++ webapp/src/main/java/com/infoshare/fourfan/servlet/ProductListServlet.java
@@ -1,5 +1,6 @@
 package com.infoshare.fourfan.servlet;
 
+import com.infoshare.fourfan.dto.ProductDto;
 import com.infoshare.fourfan.freemarker.TemplateProvider;
 import com.infoshare.fourfan.service.ProductService;
 import com.infoshare.fourfan.service.ProductServiceDb;
@@ -11,9 +12,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -35,8 +41,15 @@ public class ProductListServlet extends HttpServlet {
         Template template = templateProvider.getTemplate(getServletContext(), "productList.ftlh");
         PrintWriter printWriter = resp.getWriter();
 
+        List<ProductDto> productDtos = ClientBuilder
+                .newClient()
+                .target(UriBuilder.fromPath("http://127.0.0.1:8080/resources/products"))
+                .request(MediaType.APPLICATION_JSON)
+                .get()
+                .readEntity(new GenericType<List<ProductDto>>() {});
+
         Map<String, Object> dataModel = new HashMap<>();
-        dataModel.put("products", productServiceDb.findAll());
+        dataModel.put("products", productDtos);
 
         try {
             template.process(dataModel, printWriter);

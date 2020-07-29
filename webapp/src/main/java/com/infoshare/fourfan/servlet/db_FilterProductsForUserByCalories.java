497857eac9d60dd1741db00c021e35bb57cc1d97
AdamSkretkowicz <adam.skretkowicz@gmail.com>
2020-07-29 23:26:33 +0200

    db podstawa - przywrocenie radio dla filtrowania po kaloryce

--- webapp/src/main/java/com/infoshare/fourfan/servlet/db_FilterProductsForUserByCalories.java
+++ webapp/src/main/java/com/infoshare/fourfan/servlet/db_FilterProductsForUserByCalories.java
@@ -22,44 +22,93 @@ import java.util.logging.Logger;
 @WebServlet("/db_filterUserProductsByCalories")
 public class db_FilterProductsForUserByCalories extends HttpServlet {
 
-        @Inject
-        private db_ProductDao db_productDao;
+    public enum CaloriesRangeEnum {
+        RANGE_0_150,
+        RANGE_151_300,
+        RANGE_FROM_301;
+    }
 
-        private static final Logger logger
-                = Logger.getLogger(FilterProductsForUserByCalories.class.getName());
+    @Inject
+    private db_ProductDao db_productDao;
 
-        @Inject
-        private TemplateProvider templateProvider;
+    private static final Logger logger = Logger.getLogger(FilterProductsForUserByCalories.class.getName());
 
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-            resp.setContentType("text/html;charset=UTF-8");
-            PrintWriter printWriter = resp.getWriter();
+    @Inject
+    private TemplateProvider templateProvider;
 
-            String minCalories = req.getParameter("min");
-            String maxCalories = req.getParameter("max");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter printWriter = resp.getWriter();
 
-            Template template = templateProvider.getTemplate(getServletContext(), "db_filterByCalories.ftlh");
-            Map<String, Object> dataModel = new HashMap<>();
+        String calories = req.getParameter("calories");
 
 
-            if(minCalories != null && maxCalories != null) {
-                Integer minCaloriesInt = Integer.parseInt(minCalories);
-                Integer maxCaloriesInt = Integer.parseInt(maxCalories);
-                Optional<List<db_ProductDto>> db_product = db_productDao.findProductCaloriesDto(minCaloriesInt,maxCaloriesInt);
+        Template template = templateProvider.getTemplate(getServletContext(), "db_filterByCalories.ftlh");
+        Map<String, Object> dataModel = new HashMap<>();
+        dataModel.put("firstRange", CaloriesRangeEnum.RANGE_0_150);
+        dataModel.put("secondRange", CaloriesRangeEnum.RANGE_151_300);
+        dataModel.put("thirdRange", CaloriesRangeEnum.RANGE_FROM_301);
 
-                if (db_product.isPresent()){
-                    dataModel.put("products", db_product.get());
-                } else {
-                    dataModel.put("errorMessage", "Product has not been found.");
-                }
-            }
+        if(calories != null && !calories.equals("Przedzial") ) {
+            CaloriesRangeEnum caloriesRangeEnum = CaloriesRangeEnum.valueOf(calories);
+            CaloriesRange caloriesRange = new CaloriesRange(caloriesRangeEnum).invoke();
+            int productMin = caloriesRange.getProductMin();
+            int productMax = caloriesRange.getProductMax();
 
-            try {
-                template.process(dataModel, printWriter);
-            } catch (TemplateException e) {
-                logger.severe(e.getMessage());
+            Optional<List<db_ProductDto>> db_product = db_productDao.findProductCaloriesDto(productMin,productMax);
+
+            if (db_product.isPresent()){
+                dataModel.put("products", db_product.get());
+            } else {
+                dataModel.put("errorMessage", "Product has not been found.");
             }
         }
 
+        try {
+            template.process(dataModel, printWriter);
+        } catch (TemplateException e) {
+            logger.severe(e.getMessage());
+        }
+    }
+
+    private static class CaloriesRange {
+        private CaloriesRangeEnum caloriesRange;
+        private int productMin;
+        private int productMax;
+
+        public CaloriesRange(CaloriesRangeEnum caloriesRange) {
+            this.caloriesRange = caloriesRange;
+        }
+
+        public int getProductMin() {
+            return productMin;
+        }
+
+        public int getProductMax() {
+            return productMax;
+        }
+
+        public CaloriesRange invoke() {
+            productMin = -1;
+            productMax = 1000000000;
+
+            switch (caloriesRange) {
+                case RANGE_0_150:
+                    productMin = 0;
+                    productMax = 150;
+                    break;
+                case RANGE_151_300:
+                    productMin = 151;
+                    productMax = 300;
+                    break;
+                case RANGE_FROM_301:
+                    productMin = 301;
+                    productMax = 100000000;
+                    break;
+            }
+            return this;
+        }
+    }
 }
+
